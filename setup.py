ff0c988b8e2ba3a60a0817716911304feed82efb
dudushy <eduardotalarico1337@gmail.com>
2023-05-29 16:28:31 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+# pip freeze | xargs pip uninstall -y
+# pip freeze > requirements.txt
+#* -- Imports
+import os
+
+os.system("pip install -r requirements.txt") #? Install requirements
+os.system("pause") #? Send "pause" to system (Press any key to continue . . .)
