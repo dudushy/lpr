05fe366e178d4ff4758390b9dd5e7f617255dbfe
dudushy <eduardotalarico1337@gmail.com>
2023-05-31 18:53:37 -0300

    Fix main code + README

--- src/main.py
+++ src/main.py
@@ -5,12 +5,18 @@ import os
 import glob
 import sys
 
+from lpr.lpr import LPR
+from imutils import paths
+import argparse
+import imutils
+import cv2
+
 
 # * -- Variables
 title = "LPR"
 path = os.path.dirname(__file__)  # ? Directory path
-assets_path = path + r"\assets\samples\images\*"  # ? Assets path
-output_folder_path = path + r"\output"  # ? Output folder path
+assets_path = path + "/assets/samples/images/*"  # ? Assets path
+output_folder_path = path + "/output"  # ? Output folder path
 
 # * -- Functions
 
@@ -25,8 +31,18 @@ def checkPaths() -> None:
     print(f"[{title}#checkPaths] output_folder_path: ", output_folder_path)
 
 
+def cleanup_text(text):
+	return "".join([c if ord(c) < 128 else "" for c in text]).strip()
+
 def lpr() -> None:
     print(f"[{title}#lpr] init")
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--psm", type=int, default=7,
+        help="default PSM mode for OCR'ing license plates")
+    args = vars(ap.parse_args())
+
+    lprOperator = LPR(debug=False)
 
     files = glob.iglob(assets_path)
     print(f"[{title}#lpr] files: ", files)
@@ -36,24 +52,31 @@ def lpr() -> None:
             print("- - - - - - - - - - - - - - - - - - - - -")
             print(f"[{title}#lpr] file: ", file)
 
-            filename = file.split("\\")[-1].split(".")[0]
+            filename = file.split("/")[-1].split(".")[0]
             print(f"[{title}#lpr] filename: ", filename)
 
-            output = "temp"
-            print(f"[{title}#lpr] ({filename}) output: ", output)
+            image = cv2.imread(file)
+            image = imutils.resize(image, width=600)
+            (lpText, lpCnt) = lprOperator.find_and_ocr(image, psm=args["psm"],
+                clearBorder=False)
 
-            output_destination = output_folder_path + f"\\{filename}.txt"
-            print(f"[{title}#lpr] output_destination: ", output_destination)
+            if lpText is not None:
+                # output = "temp"
+                output = cleanup_text(lpText)
+                print(f"[{title}#lpr] ({filename}) output: ", output)
 
-            outputFolderExists = os.path.exists(output_folder_path)
-            print(f"[{title}#lpr] outputFolderExists: ", outputFolderExists)
+                output_destination = output_folder_path + f"/{filename}.txt"
+                print(f"[{title}#lpr] output_destination: ", output_destination)
 
-            if not outputFolderExists:
-                print(f"[{title}#lpr] create output folder")
-                os.mkdir(output_folder_path)
+                outputFolderExists = os.path.exists(output_folder_path)
+                print(f"[{title}#lpr] outputFolderExists: ", outputFolderExists)
 
-            with open(output_destination, "w", encoding="utf-8") as result:
-                result.write(output)
+                if not outputFolderExists:
+                    print(f"[{title}#lpr] create output folder")
+                    os.mkdir(output_folder_path)
+
+                with open(output_destination, "w", encoding="utf-8") as result:
+                    result.write(output)
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -71,6 +94,8 @@ def main() -> None:
 
     checkPaths()
     lpr()
+
+    print(f"[{title}#main] exit")
 
 
 if __name__ == "__main__":
